a1f99c81a75decd5afc705acf4a94bba7852164f
Fougeray Florian <fougeray.florian@gmail.com>
2022-09-15 17:15:53 +0200

    form bilan type WIP

--- script/app.js
+++ script/app.js
@@ -1,6 +1,23 @@
 const app = {
+  // ------------------------------------------------------------------
+  // Datas
+  bilanApo: bilanApo,
+
+  // ------------------------------------------------------------------
+  // Dom element
+  appContainer: document.getElementById("app"),
+
+  // ------------------------------------------------------------------
+  // Methods
   init: () => {
-    console.log("app init ok");
+    console.log("app init ok ");
+  },
+
+
+  
+  getBilanType: (e) => {
+    const bilanType = e.target.value
+    console.log(bilanType);
   },
 };
 
---
+++ script/data.js
@@ -0,0 +1,95 @@
+const bilanApo = {
+  // ------------
+  introduction: {
+    highLevel: [
+      "Introduction highLevel 1",
+      "Introduction highLevel 2",
+      "Introduction highLevel 3",
+    ],
+    midelLevel: [
+      "Introduction midelLevel 1",
+      "Introduction midelLevel 2",
+      "Introduction midelLevel 3",
+    ],
+    lowLevel: [
+      "Introduction lowLevel 1",
+      "Introduction lowLevel 2",
+      "Introduction lowLevel 3",
+    ],
+  },
+
+  // ------------
+  gitParticipation: {
+    perfect: [
+      "gitParticipation perfect 1",
+      "gitParticipation perfect 2",
+      "gitParticipation perfect 3",
+    ],
+    average: [
+      "gitParticipation average 1",
+      "gitParticipation average 2",
+      "gitParticipation average 3",
+    ],
+    low: [
+      "gitParticipation low 1",
+      "gitParticipation low 2",
+      "gitParticipation low 3",
+    ],
+  },
+
+  // ------------
+  outro: {
+    highLevel: ["outro highLevel 1", "outro highLevel 2", "outro highLevel 3"],
+    midLevel: ["outro midLevel 1", "outro midLevel 2", "outro midLevel 3"],
+    lowLevel: ["outro lowLevel 1", "outro lowLevel 2", "outro lowLevel 3"],
+  },
+};
+
+// CLEF DES OBJET A MODIFIER
+
+const bilanFormation = {
+  // ------------
+  introduction: {
+    highLevel: [
+      "Introduction highLevel 1",
+      "Introduction highLevel 2",
+      "Introduction highLevel 3",
+    ],
+    midelLevel: [
+      "Introduction midelLevel 1",
+      "Introduction midelLevel 2",
+      "Introduction midelLevel 3",
+    ],
+    lowLevel: [
+      "Introduction lowLevel 1",
+      "Introduction lowLevel 2",
+      "Introduction lowLevel 3",
+    ],
+  },
+
+  // ------------
+  gitParticipation: {
+    perfect: [
+      "gitParticipation perfect 1",
+      "gitParticipation perfect 2",
+      "gitParticipation perfect 3",
+    ],
+    average: [
+      "gitParticipation average 1",
+      "gitParticipation average 2",
+      "gitParticipation average 3",
+    ],
+    low: [
+      "gitParticipation low 1",
+      "gitParticipation low 2",
+      "gitParticipation low 3",
+    ],
+  },
+
+  // ------------
+  outro: {
+    highLevel: ["outro highLevel 1", "outro highLevel 2", "outro highLevel 3"],
+    midLevel: ["outro midLevel 1", "outro midLevel 2", "outro midLevel 3"],
+    lowLevel: ["outro lowLevel 1", "outro lowLevel 2", "outro lowLevel 3"],
+  },
+};
