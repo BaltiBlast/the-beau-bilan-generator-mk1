ad3a1dc0d23765ca2bb109d37e469ee99c1d5079
Fougeray Florian <fougeray.florian@gmail.com>
2022-09-16 00:40:28 +0200

    il est surtout l'heure de dormrir.WIP - refacto dynamisation

--- script/app.js
+++ script/app.js
@@ -6,31 +6,83 @@ const app = {
   // ------------------------------------------------------------------
   // Dom element
   appContainer: document.getElementById("app"),
+  form: document.getElementById("categorieBilan"),
 
   // ------------------------------------------------------------------
   // Methods
   init: () => {
-    console.log(app.data);
     app.getBilanType();
   },
 
   getBilanType: () => {
-    // Dom element
-    const categorieBilanContainer = document.getElementById("categorieBilan");
+    const categorieBilanContainer = document.getElementById("selectBilanType");
 
-    // Option in selectBilanType
-    for (const bilan of data) {
-      let optionBilanType = document.createElement("option");
-      optionBilanType.innerText = bilan.name;
+    for (let i = 0; i < data.length; i++) {
+      // create option select
+      let optionBilanName = document.createElement("option");
 
-      bilan.categories.forEach((e) => {
-        let bilanInputCategorie = document.createElement("h3");
-        bilanInputCategorie.innerText = e.name
-        categorieBilanContainer.append(bilanInputCategorie);
-      });
+      // data option select
+      optionBilanName.innerHTML = data[i].name;
+      optionBilanName.setAttribute("value", data[i].name.toLowerCase());
 
-      selectBilanType.append(optionBilanType);
+      // add option in select
+      categorieBilanContainer.append(optionBilanName);
     }
+
+    // onchange event on select
+    categorieBilanContainer.addEventListener("change", (ele) => {
+      // get value option
+      let optionValue = ele.target.value;
+
+      // returning form by value
+      switch (optionValue) {
+        case "fondations":
+          app.form.innerHTML = ""
+          app.fondationsForm();
+          break;
+
+        case "approfondissement":
+          app.form.innerHTML = ""
+          app.approfondissementForm();
+          break;
+
+        case "apothéose":
+          app.form.innerHTML = ""
+          console.log("switch apothéose");
+          break;
+
+        case "fin de formation":
+          app.form.innerHTML = ""
+          console.log("switch fin de forma");
+          break;
+
+        default:
+          console.log("switch y a R ");
+      }
+    });
+  },
+
+  fondationsForm: () => {
+    const dataFondations = data.slice(0, 1);
+
+    dataFondations.forEach((element) => {
+      for (const categorie of element.categories) {
+        let title = document.createElement("h3");
+        title.innerText = categorie.name;
+        app.form.append(title);
+      }
+    });
+  },
+
+  approfondissementForm: () => {
+    const dataApprofondissement = data.slice(1, 2);
+    dataApprofondissement.forEach((element) => {
+      for (const categorie of element.categories) {
+        let title = document.createElement("h3");
+        title.innerText = categorie.name;
+        app.form.append(title);
+      }
+    });
   },
 };
 
