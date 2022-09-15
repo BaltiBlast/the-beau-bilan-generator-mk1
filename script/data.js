7a760da63ca486e52f9821663ccfb483e68873a7
Charly Morand <charly,morand@oclock.io>
2022-09-15 18:50:59 +0200

    added data

--- script/data.js
+++ script/data.js
@@ -31,7 +31,7 @@ const data = [
       {name: "Introduction",
       levels: [
         {level: 1,
-         name: "Étudiant à l'aise, bon niveau",
+        name: "Étudiant à l'aise, bon niveau",
         notes: [
           {id: 1,
           note: `Ça y est, la folle aventure O'clock s'achève et tu t'en es sorti comme un chef [studentName]!`},
@@ -73,72 +73,108 @@ const data = [
           {id: 7,
           note: `Te voici [arrive.ee] au bout de ces 5 mois de formation [studentName]. Ce ne fut pas de tout repos, néanmois bravo à toi pour avoir le coup jusqu'au bout !`},
         ]},
-      ]
-    },
+      ]},
       {name: "Évolution technique",
       levels: [
         {level: 1,
         name: "Tout est réussi",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
+          note: `Tu as le soucis du détail et du travail bien fait, et tu ne laisse rien au hasard. Tes parcours sont exemplaires. `},
+          {id: 2,
+          note: `Trouver une phrase`},
+          {id: 3,
+          note: `Trouver une phrase`},
         ]},
-        {level: 1,
+        {level: 2,
         name: "C'est en progression",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
+          note: `Je tiens à te féliciter pour la motivation dont tu as fait preuve pendant cette formation. Ce n'était pas de tout repos mais tu as tenu bon en te donnant au maximum pour réussir et on note une très bonne progression tout le long de la formation.`},
+          {id: 2,
+          note: `Trouver une phrase`},
+          {id: 3,
+          note: `Trouver une phrase`},
         ]},
-        {level: 1,
-        name: "",
+        {level: 3,
+        name: "En dent de scie",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
+          note: `Ça n'a pas été de tout repos pour toi, avec des parcours mieux réussis que d'autres. `},
+          {id: 2,
+          note: `Trouver une phrase`},
+          {id: 3,
+          note: `Trouver une phrase`},
         ]},
-        {level: 1,
-        name: "",
+        {level: 4,
+        name: "En diminution graduelle",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
+          note: `Tu as bien réussis les parcours au départ, mais a eu de plus en mal de mal à chaque parcours. `},
+          {id: 2,
+          note: `Trouver une phrase`},
+          {id: 3,
+          note: `Trouver une phrase`},
         ]},
-        {level: 1,
-        name: "",
+        {level: 5,
+        name: "C'est la galère partout",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
+          note: `La formation a été difficile pour toi, avec beaucoup de parcours non aboutis.`},
+          {id: 2,
+          note: `Trouver une phrase`},
+          {id: 3,
+          note: `Trouver une phrase`},
         ]},
-      ]
-    },
-      {name: "Notions",
-      levels: [
-        {level: 1,
-        name: "",
-        notes: [
-          {id: 1,
-          note: `Ajouter une phrase ici`}
-        ]}
-      ]
-    },
+      ]},
       {name: "Vie dans la promo",
       levels: [
         {level: 1,
-        name: "",
+        name: "Moteur en cockpit et sur Slack",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
-        ]}
-      ]
-    },
+          note: `Ta participation a été exemplaire tout du long en cockpit et sur slack. Tu as été d'une grande aide pour les autres apprenants, et tu as participé à la bonne ambiance de la classe. Nul doute que tu en as toi retiré beaucoup de bénéfices !`}
+        ]},
+        {level: 2,
+        name: "Moteur sur Slack mais timide en cockpit",
+        notes: [
+          {id: 1,
+          note: `Tu as eu une super participation sur Slack où tu n'as pas hésité à poser des questions et aider les autres. Tu as été un peu plus discrèt en cockpi, c'est dommage car tu aurais pu poser des questions supplémentaires au prof ou tenter d'aider les autres ce qui est très bénéfique. `}
+        ]},
+        {level: 3,
+        name: "Moteur en cockpit mais timide sur Slack",
+        notes: [
+          {id: 1,
+          note: `Tu as été actif en cockpit, en n'hésitant pas à aider les autres ou à poser des questions, c'est super ! Par contre tu as été plus discrèt sur slack, c'est dommage car ça peut te permettre de participer à des discussions intéressantes ou de toi même en lancer.`}
+        ]},
+        {level: 1,
+        name: "Discret sur les deux",
+        notes: [
+          {id: 1,
+          note: `Tu as été discrèt pendant toute la formation. C'est dommage car tu n'as pas profiter à 100% de l'intérêt d'avoir un formateur et une communauté d'apprenant au bout de ton clavier. Pour la suite, je te conseille d'être plus proactif sur ce point, ça te permettra d'obtenir des réponses à des questions que tu peux avoir ou bien d'échanger sur des sujets intéressants qui t'apporteront beaucoup.`}
+        ]},
+      ]},
       {name: "Conseils projet pro",
       levels: [
         {level: 1,
-        name: "",
+        name: "Souhaite passer le TP prochainement",
         notes: [
           {id: 1,
-          note: `Ajouter une phrase ici`}
-        ]}
-      ]
-    },
+          note: `Vu que tu souhaites passer le TP prochainement, nous te conseillons de retravailler sur le projet d'apothéose afin de t'approprier le code réalisé par ton groupe.`}
+        ]},
+        {level: 1,
+        name: "Souhaite passer le TP plus tard",
+        notes: [
+          {id: 1,
+          note: `Vu que tu souhaites passer le TP, nous te conseillons de retravailler sur le projet d'apothéose afin de t'approprier le code réalisé par ton groupe. Mais tu peux aussi prendre le temps de refaire un projet qui te sera propre si tu le souhaites, tu as toutes les cartes en main pour y parvenir.`}
+        ]},
+        {level: 1,
+        name: "Autre",
+        notes: [
+          {id: 1,
+          note: `Ta formation est finie mais pas ton apprentissage ! N'hésite pas à reprendre les challenges pour les maîtriser comme il se doit et continuer ta progression.`}
+        ]},
+      ]},
       {name: "Conclusion",
       levels: [
         {level: 1,
@@ -147,7 +183,44 @@ const data = [
           {id: 1,
           note: `Ajouter une phrase ici`}
         ]}
-      ]
-    },
+      ]},
+      {name: "Félicitations/Encouragements",
+      levels: [
+        {level: 1,
+        name: "",
+        notes: [
+          {id: 1,
+          note: `Ajouter une phrase ici`}
+        ]}
+      ]},
+      {name: "Conclusion",
+      levels: [
+        {level: 1,
+        name: "Étudiant à l'aise, bon niveau",
+        notes: [
+          {id: 1,
+          note: `Avec ce que tu nous as proposé durant toute la formation, nous ne sommes vraiment pas inquiet pour la suite !`},
+          {id: 1,
+          note: `Ton futur employeur aura de la chance de t'avoir dans son équipe ! Je te souhaite de t'amuser dans ton futur job.`},
+        ]},
+        {level: 2,
+        name: "Étudiant correct, des points à améliorer",
+        notes: [
+          {id: 1,
+          note: `Ta rigueur et ton bon apprentissage font de toi un bon étudiant. Garde le cap et tu deviendras un bon développeur !`}
+        ]},
+        {level: 3,
+        name: "Étudiant avec un certain retard, niveau moyen",
+        notes: [
+          {id: 1,
+          note: `Avec une formation aussi intensive, normal de ne pas tout maîtriser sous le bout des doigts. Laisse toi un peu de temps et de pratique pour comprendre que tu te débrouilles au final pas si mal !`}
+        ]},
+        {level: 4,
+        name: "Étudiant en difficulté, niveau faible",
+        notes: [
+          {id: 1,
+          note: `Tu as su démontrer tout au long de la formation que tu étais capable de t'accrocher malgré la difficulté. C'est une qualité importante quand on est développeur, avec du temps et du travail tu finiras par être à l'aise !`}
+        ]},
+      ]},
   ]},
 ]
